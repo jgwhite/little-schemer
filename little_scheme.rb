83aee43afedc56bebcc3e90a44a3bb487342a94e
Jamie White <jamie@jgwhite.co.uk>
2014-01-05 20:47:32 +0000

    Secondary try, with lambda

--- little_scheme.rb
+++ little_scheme.rb
@@ -2,22 +2,143 @@ require 'treetop'
 Treetop.load('little_scheme.treetop')
 
 module LittleScheme
-  def self.evaluate(program)
-    parse(program).evaluate(env).last
+  def self.parse(source)
+    LittleSchemeParser.new.parse(source).to_ast
   end
 
-  def self.parse(program)
-    LittleSchemeParser.new.parse(program)
-  end
+  def self.run(source)
+    ast = parse(source)
+    env = PRIMITIVES
 
-  def self.env
-    {
-      car: -> (l) { l[0] if l.is_a?(Array) },
-      cdr: -> (l) { l[1..-1] if l.is_a?(Array) },
-      cons: -> (s, l) { [s] + l if l.is_a?(Array) },
-      null?: -> (l) { l == [] },
-      atom?: -> (l) { l.is_a?(Symbol) },
-      eq?: -> (a, b) { a == b }
+    results = ast.map { |s|
+      env, result = evaluate(env, s)
+      result
     }
+
+    results.last
   end
+
+  def self.evaluate(env, s)
+    case s
+    when Numeric, [] then [env, s]
+    when Array then evaluate_list(env, s)
+    when Symbol then evaluate_atom(env, s)
+    end
+  end
+
+  def self.evaluate_list(env, list)
+    car, *cdr = list
+    env, car = evaluate(env, car)
+
+    if car.respond_to?(:call)
+      car.call(env, *cdr)
+    else
+      results = list.map do |s|
+        env, result = evaluate(env, s)
+        result
+      end
+
+      [env, results]
+    end
+  end
+
+  def self.evaluate_atom(env, atom)
+    result = env[atom] || atom
+
+    [env, result]
+  end
+
+  PRIMITIVES = {
+    car: -> (env, s) {
+      env, s = evaluate(env, s)
+
+      if s.is_a?(Array)
+        [env, s.first]
+      else
+        [env, nil]
+      end
+    },
+
+    cdr: -> (env, s) {
+      env, s = evaluate(env, s)
+
+      if s.is_a?(Array)
+        [env, s[1..-1]]
+      else
+        [env, nil]
+      end
+    },
+
+    cons: -> (env, s, l) {
+      env, l = evaluate(env, l)
+
+      if l.is_a?(Array)
+        env, s = evaluate(env, s)
+
+        [env, [s] + l]
+      end
+    },
+
+    null?: -> (env, s) {
+      env, s = evaluate(env, s)
+
+      if s.is_a?(Array)
+        [env, s == []]
+      else
+        [env, nil]
+      end
+    },
+
+    pair?: -> (env, s) {
+      env, s = evaluate(env, s)
+
+      [env, s.is_a?(Array)]
+    },
+
+    and: -> (env, *clauses) {
+      result = clauses.all? { |clause|
+        env, r = evaluate(env, clause)
+        r
+      }
+
+      [env, result]
+    },
+
+    not: -> (env, s) {
+      env, result = evaluate(env, s)
+
+      [env, !result]
+    },
+
+    eq?: -> (env, a, b) {
+      env, a = evaluate(env, a)
+      env, b = evaluate(env, b)
+
+      [env, a == b]
+    },
+
+    define: -> (env, a, s) {
+      env, a = evaluate(env, a)
+      env, s = evaluate(env, s)
+
+      env = env.merge(a => s)
+
+      [env, nil]
+    },
+
+    lambda: -> (env, names, s) {
+      result = -> (_, *values) {
+        scope = env.merge(Hash[names.zip(values)])
+        _, result = evaluate(scope, s)
+
+        [env, result]
+      }
+
+      [env, result]
+    },
+
+    quote: -> (env, s) {
+      [env, s]
+    }
+  }
 end
--- little_scheme_test.rb
+++ little_scheme_test.rb
@@ -3,66 +3,133 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class LittleSchemeTest < MiniTest::Unit::TestCase
-  def assert_result(result, program)
-    assert_equal result, LittleScheme.evaluate(program)
+  def assert_ast(result, source)
+    assert_equal result, LittleScheme.parse(source)
   end
 
-  def test_atoms
-    assert_result :atom, 'atom'
-    assert_result :turkey, 'turkey'
-    assert_result :'1492', '1492'
-    assert_result :u, 'u'
+  def assert_result(result, source)
+    assert_equal result, LittleScheme.run(source)
   end
 
-  def test_lists
-    assert_result [], '()'
-    assert_result [:atom], '(atom)'
-    assert_result [:atom, :turkey, :or], '(atom turkey or)'
-    assert_result [[:atom, :turkey], :or], '((atom turkey) or)'
+  def test_parse
+    assert_ast [:atom], "atom"
+    assert_ast [:turkey], "turkey"
+    assert_ast [1492], "1492"
+    assert_ast [:u], "u"
+    assert_ast [:"*abc$"], "*abc$"
+    assert_ast [[:atom]], "(atom)"
+    assert_ast [[:atom, :turkey, :or]], "(atom turkey or)"
+    assert_ast [[:atom, :turkey], :or], "(atom turkey) or"
+    assert_ast [[[:x, :y], :z]], "((x y) z)"
+    assert_ast [[]], "()"
+    assert_ast [[[], [], [], []]], "(() () () ())"
   end
 
   def test_car
-    assert_result :a, '(car (a b c))'
-    assert_result [:a, :b, :c], '(car ((a b c) x y z))'
-    assert_result nil, '(car hotdog)'
-    assert_result nil, '(car ())'
-    assert_result [[:hotdogs]], '(car (((hotdogs)) (and) (pickle) relish))'
-    assert_result [:hotdogs], '(car (car (((hotdogs)) (and))))'
+    assert_result :a, "(car (a b c))"
+    assert_result nil, "(car hotdog)"
+    assert_result nil, "(car ())"
   end
 
   def test_cdr
-    assert_result [:b, :c], '(cdr (a b c))'
-    assert_result [:x, :y, :z], '(cdr ((a b c) x y z))'
-    assert_result [], '(cdr (hamburger))'
-    assert_result [:t, :r], '(cdr ((x) t r))'
-    assert_result nil, '(cdr hotdogs)'
-    assert_result nil, '(cdr ())'
-    assert_result [:x, :y], '(car (cdr ((b) (x y) ((c)))))'
-    assert_result [[[:c]]], '(cdr (cdr ((b) (x y) ((c)))))'
-    assert_result nil, '(cdr (car (a (b (c)) d)))'
+    assert_result [:b, :c ], "(cdr (a b c))"
+    assert_result nil, "(cdr hotdogs)"
+    assert_result nil, "(cdr ())"
   end
 
   def test_cons
-    assert_result [:peanut, :butter, :and, :jelly],
-                  '(cons peanut (butter and jelly))'
-    assert_result [[:banana, :and], :peanut, :butter, :and, :jelly],
-                  '(cons (banana and) (peanut butter and jelly))'
-    assert_result nil, '(cons ((a b c)) b)'
+    assert_result [:peanut, :butter, :jelly],
+                  "(cons peanut (butter jelly))"
+    assert_result [[:banana], :peanut, :butter, :jelly],
+                  "(cons (banana) (peanut butter jelly))"
+    assert_result nil, "(cons ((a b c)) b)"
+    assert_result nil, "(cons a b)"
   end
 
-  def test_null?
-    assert_result true, '(null? ())'
-    assert_result false, '(null? (a b c))'
-    assert_result false, '(null? a)'
+  def test_null
+    assert_result true, "(null? ())"
+    assert_result true, "(null? (quote ()))"
+    assert_result true, "(null? '())"
+    assert_result false, "(null? (a b c))"
+    assert_result nil, "(null? a)"
   end
 
-  def test_atom?
-    assert_result true, '(atom? Harry)'
-    assert_result false, '(atom? (Harry had a heap of apples))'
+  def test_not
+    assert_result true, "(not (null? a))"
+    assert_result false, "(not (null? ()))"
   end
 
-  def test_eq?
-    assert_result true, '(eq? Harry Harry)'
-    assert_result false, '(eq? margarine butter)'
+  def test_pair
+    assert_result true, "(pair? ())"
+    assert_result false, "(pair? a)"
+  end
+
+  def test_define
+    assert_result [:b, :c], %{
+      (define a (b c))
+      a
+    }
+  end
+
+  def test_lambda
+    definition = %{
+      (define foo
+        (lambda (x)
+          (bar x)))
+    }
+
+    assert_result [:bar, :a], %{
+      #{definition}
+      (foo a)
+    }
+
+    assert_result :x, %{
+      #{definition}
+      (foo a)
+      x
+    }
+  end
+
+  def test_atom
+    definition = %{
+      (define atom?
+        (lambda (x)
+          (and (not (pair? x)) (not (null? x)))))
+    }
+
+    assert_result true, %{
+      #{definition}
+      (atom? a)
+    }
+
+    assert_result false, %{
+      #{definition}
+      (atom? ())
+    }
+  end
+
+  def test_eq
+    assert_result true, "(eq? Harry Harry)"
+    assert_result false, "(eq? margarine butter)"
+    assert_result false, %{
+      (define l1 ())
+      (define l2 (strawberry))
+
+      (eq? l1 l2)
+    }
+    assert_result true, "(eq? (a) (a))"
+    assert_result true, "(eq? 7 7)"
+    assert_result false, "(eq? 6 7)"
+    assert_result false, %{
+      (define l (soured milk))
+      (define a milk)
+
+      (eq? (cdr l) a)
+    }
+    assert_result true, %{
+      (define l (beans beans we need jelly beans))
+
+      (eq? (car l) (car (cdr l)))
+    }
   end
 end
